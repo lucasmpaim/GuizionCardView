c61938f138a8d8b68473f8a29d6e94853fce027e
Lucas Paim <lucasmpaim1@gmail.com>
2016-12-08 15:44:52 -0200

    layout fix

--- guizion-card-view/Classes/FrontView.swift
+++ guizion-card-view/Classes/FrontView.swift
@@ -38,7 +38,7 @@ class CardFrontView: UIImageView {
         self.addSubview(cardNumber)
         self.addSubview(cardName)
         self.addSubview(validateNumber)
-        cardNumber.font = UIFont(name: Constants.fontName, size: 20.0)
+        cardNumber.font = UIFont(name: Constants.fontName, size: 22.0)
         validateNumber.font = UIFont(name: Constants.fontName, size: 14.0)
         cardName.font = UIFont(name: Constants.fontName, size: 14.0)
     }
